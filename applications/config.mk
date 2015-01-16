e9fc147c15a95db0c53c2cbb2a774717d0864247
Bradley Thwaites <bthwaites@gatech.edu>
2015-01-16 17:21:28 -0500

    smarter defaults

--- applications/config.mk
+++ applications/config.mk
@@ -1,6 +1,7 @@
-FANN_INC	:= /filespace/people/y/yazdanbakhsh/FANN/bin/include
-FANN_LIB	:= /filespace/people/y/yazdanbakhsh/FANN/bin/lib
-PARROT_LIB  := /home/yazdan/phd/projects/parrot.cpu/parrot.c/src/ParrotLib
-PLANG 		:= /home/yazdan/phd/projects/parrot.cpu/parrot.c/src/ParrotObserver/plang.py
-PARROT_JSON := /home/yazdan/phd/projects/parrot.cpu/parrot.c/src/ParrotObserver/ParrotC.json
+BASE_DIR    := /home/bradley/Projects/axbench
+FANN_INC	:= /usr/local/include
+FANN_LIB	:= /usr/local/lib
+PARROT_LIB  := $(BASE_DIR)/parrot.c/src/ParrotLib
+PLANG 		:= $(BASE_DIR)/parrot.c/src/ParrotObserver/plang.py
+PARROT_JSON := $(BASE_DIR)/parrot.c/src/ParrotObserver/ParrotC.json
 
